c70f086dd4ebba13b0a8fe1748f52ede85854c2a
Ulises Gascon <ulisesgascongonzalez@gmail.com>
2015-10-14 16:48:04 +0200

    package.json and config file added

---
+++ config.js
@@ -0,0 +1,7 @@
+var config = {
+	token: "", // Firebase Token
+	firebaseApp: "", // https://<< --- Firebase APP URL --- >>.firebaseio.com
+	firebaseAppRuta: "" // https://<< --- Firebase APP URL --- >>.firebaseio.com/<--- Ruta --->/
+};
+
+module.exports = config;
