497dfdca5c034c7ef06c3d3df84eb7d9ba1118f5
Ulises Gascón <ulisesgascongonzalez@gmail.com>
2016-04-24 19:13:49 +0200

    Update server.js

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(express.static(path.join(__dirname, 'publico')));
 // CORS
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
